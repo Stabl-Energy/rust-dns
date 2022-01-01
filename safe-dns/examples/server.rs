be8a429a9d705dc3b6105644eb39a7de214d0601
Michael Leonhard <michael@leonhardllc.com>
2022-01-01 13:15:20 -0800

    safe-dns: Add example/server.

---
+++ safe-dns/examples/server.rs
@@ -0,0 +1,25 @@
+// $ cargo run --package safe-dns --example server
+
+use permit::Permit;
+use safe_dns::DnsRecord;
+use std::io::Read;
+use std::net::{IpAddr, Ipv6Addr, SocketAddr, UdpSocket};
+
+fn main() {
+    let permit = Permit::new();
+    let serve_udp_permit = permit.new_sub();
+    let sock = UdpSocket::bind(SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), 0)).unwrap();
+    let addr = sock.local_addr().unwrap();
+    println!("Bound UDP socket {}", addr.port());
+    let records = vec![
+        DnsRecord::new_a("hello.example.com", "10.0.0.1").unwrap(),
+        DnsRecord::new_aaaa("hello.example.com", "2606:2800:220:1:248:1893:25c8:1946").unwrap(),
+        DnsRecord::new_cname("abc.example.com", "def.example.com").unwrap(),
+    ];
+    let join_handle = std::thread::spawn(move || {
+        safe_dns::serve_udp(&serve_udp_permit, &sock, &records).unwrap()
+    });
+    while std::io::stdin().read(&mut [0u8]).is_ok() {}
+    permit.revoke();
+    join_handle.join().unwrap();
+}
