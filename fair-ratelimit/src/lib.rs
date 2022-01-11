7392026bd799a0c4da6bfe0f350c26b474b51cf0
Michael Leonhard <michael@leonhardllc.com>
2022-01-10 16:51:06 -0800

    fair-ratelimit: Simplify simulation types.  First version of limiter fn.

--- fair-ratelimit/src/lib.rs
+++ fair-ratelimit/src/lib.rs
@@ -53,10 +53,57 @@
 
 mod ip_subnet;
 
+use core::time::Duration;
 use oorandom::Rand32;
+use std::collections::HashMap;
 use std::time::Instant;
 
 pub use ip_subnet::IpSubnet;
+
+const HORIZON_DURATION: Duration = Duration::from_secs(10);
+const TICK_DURATION: Duration = Duration::from_millis((HORIZON_DURATION.as_millis() / 10) as u64);
+
+fn right_shift<T: Clone + Default>(slice: &mut [T], n: usize) {
+    if n == 0 {
+        return;
+    }
+    let n = n.min(slice.len());
+    slice.rotate_right(n);
+    slice[0..n].fill(Default::default());
+}
+
+fn saturating_f32_to_usize(x: f32) -> usize {
+    if x == f32::NAN {
+        0
+    } else if x < 0.0 {
+        0
+    } else if x > (usize::MAX as f32) {
+        usize::MAX
+    } else {
+        x as usize
+    }
+}
+
+fn saturating_f32_to_u32(x: f32) -> u32 {
+    if x == f32::NAN {
+        0
+    } else if x < 0.0 {
+        0
+    } else if x > (u32::MAX as f32) {
+        u32::MAX
+    } else {
+        x as u32
+    }
+}
+
+trait SaturatingAddAssign<T> {
+    fn saturating_add_assign(&mut self, rhs: T);
+}
+impl SaturatingAddAssign<u32> for u32 {
+    fn saturating_add_assign(&mut self, rhs: u32) {
+        *self = self.saturating_add(rhs)
+    }
+}
 
 /// Features:
 /// - Probabilistically rejects requests.
@@ -66,25 +113,47 @@ pub use ip_subnet::IpSubnet;
 ///
 /// Implementation:
 /// - Keep a map of target IPs to the count of bytes sent recently.
-/// - Every minute, multiply the counts by a coefficient less than 1.0.
 /// - When adding a new IP to a full map, multiply the count of bytes by a random coefficient.
 ///   Create the coefficient based on the size of the smallest count, so the smallest count has
 ///   a 50% chance of getting replaced by a new packet of the same size.
 #[derive(Clone, Debug)]
 pub struct RateLimiter {
-    max_cost_per_sec: u32,
+    max_cost: u32,
+    last: Instant,
     prng: Rand32,
+    global_costs: [u32; 10],
+    source_costs: HashMap<u32, [u32; 10]>,
 }
 impl RateLimiter {
     #[must_use]
-    pub fn new(max_cost_per_sec: u32, prng: Rand32) -> Self {
+    pub fn new(max_cost_per_sec: u32, prng: Rand32, now: Instant) -> Self {
         Self {
-            max_cost_per_sec,
+            max_cost: (((max_cost_per_sec as u128) * HORIZON_DURATION.as_millis()) / 1_000) as u32,
+            last: now,
             prng,
+            global_costs: [0_u32; 10],
+            source_costs: HashMap::new(),
         }
     }
 
     pub fn check(&mut self, key: u32, cost: u32, now: Instant) -> bool {
+        let elapsed = now.saturating_duration_since(self.last);
+        let elapsed_ticks = (elapsed.as_millis() / TICK_DURATION.as_millis()) as u32;
+        self.last = self.last + (TICK_DURATION * elapsed_ticks);
+        right_shift(&mut self.global_costs, elapsed_ticks as usize);
+        let recent_cost = self
+            .global_costs
+            .iter()
+            .fold(0_u32, |acc, elem| acc.saturating_add(*elem));
+        if self.max_cost == 0 {
+            return false;
+        }
+        let recent_load = (recent_cost as f32) / (self.max_cost as f32);
+        let reject_prob = (recent_load - 0.75) * 4.0;
+        if reject_prob > 0.0 && self.prng.rand_float() < reject_prob {
+            return false;
+        }
+        self.global_costs[0].saturating_add_assign(cost);
         true
     }
 }
--- fair-ratelimit/tests/simulations.rs
+++ fair-ratelimit/tests/simulations.rs
@@ -3,12 +3,11 @@ use core::cmp::Ordering;
 use core::time::Duration;
 use fair_ratelimit::RateLimiter;
 use oorandom::Rand32;
+use std::cell::{Ref, RefCell};
 use std::collections::BinaryHeap;
+use std::fmt::{Debug, Formatter};
+use std::rc::Rc;
 use std::time::Instant;
-
-trait Client {
-    fn check(&mut self, limiter: &mut RateLimiter, now: Instant) -> Instant;
-}
 
 enum Key {
     Static(u32),
@@ -18,19 +17,28 @@ impl Key {
     pub fn get(&mut self) -> u32 {
         match self {
             Key::Static(value) => *value,
-            Key::Random(rand32_cell) => rand32_cell.get_mut().rand_u32(),
+            Key::Random(rand32_cell) => rand32_cell.get_mut().rand_range(1_000..2_000),
+        }
+    }
+}
+impl Debug for Key {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Key::Static(x) => write!(f, "Key::Static({})", x),
+            Key::Random(_) => write!(f, "Key::Random"),
         }
     }
 }
 
-struct SteadyClient {
+#[derive(Debug)]
+struct Client {
     key: Key,
     interval: Duration,
     cost: u32,
     pub accepted_requests: u32,
     pub rejected_request: u32,
 }
-impl SteadyClient {
+impl Client {
     pub fn new(key: Key, rps: u32, cost: u32) -> Self {
         Self {
             key,
@@ -40,8 +48,7 @@ impl SteadyClient {
             rejected_request: 0,
         }
     }
-}
-impl Client for SteadyClient {
+
     fn check(&mut self, limiter: &mut RateLimiter, now: Instant) -> Instant {
         if limiter.check(self.key.get(), self.cost, now) {
             self.accepted_requests += 1;
@@ -52,44 +59,47 @@ impl Client for SteadyClient {
     }
 }
 
-fn simulate<'x>(
-    num_seconds: u64,
-    limiter: &mut RateLimiter,
-    clients: &'x mut [&'x mut dyn Client],
-) {
-    struct Entry<'y>(Instant, &'y mut dyn Client);
-    impl<'y> Eq for Entry<'y> {}
-    impl<'y> PartialEq<Self> for Entry<'y> {
+fn simulate(num_seconds: u64, limiter: &mut RateLimiter, clients: &[Rc<RefCell<Client>>]) {
+    struct Entry(Instant, Rc<RefCell<Client>>);
+    impl Eq for Entry {}
+    impl PartialEq<Self> for Entry {
         fn eq(&self, other: &Self) -> bool {
             self.cmp(other) == Ordering::Equal
         }
     }
-    impl<'y> PartialOrd<Self> for Entry<'y> {
+    impl PartialOrd<Self> for Entry {
         fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
             Some(self.cmp(other))
         }
     }
-    impl<'y> Ord for Entry<'y> {
+    impl Ord for Entry {
         fn cmp(&self, other: &Self) -> Ordering {
             self.0.cmp(&other.0).reverse()
         }
     }
     let mut now = Instant::now();
     let deadline = now + Duration::from_secs(num_seconds);
-    let mut heap: BinaryHeap<Entry<'x>> = clients
-        .into_iter()
-        .map(|client| Entry(now, *client))
-        .collect();
+    let mut heap: BinaryHeap<Entry> = clients.iter().map(|rc| Entry(now, Rc::clone(rc))).collect();
+    let mut num_requests = 0;
     loop {
         let entry = heap.pop().unwrap();
-        let (new_now, client): (Instant, &'x mut dyn Client) = (entry.0, entry.1);
+        let (new_now, client): (Instant, Rc<RefCell<Client>>) = (entry.0, entry.1);
         assert!(now <= new_now);
         now = new_now;
         if deadline <= now {
             break;
         }
-        let next_request_instant = client.check(limiter, now);
+        let next_request_instant = client.borrow_mut().check(limiter, now);
         heap.push(Entry(next_request_instant, client));
+        num_requests += 1;
+    }
+    println!(
+        "Simulated {} request over {} seconds",
+        num_requests, num_seconds
+    );
+    for rc_ref_cell_client in clients {
+        let client: Ref<Client> = rc_ref_cell_client.borrow();
+        println!("client: {:?}", client);
     }
 }
 
@@ -103,54 +113,73 @@ macro_rules! assert_contains {
 
 #[test]
 fn test_single_request() {
-    let mut limiter = RateLimiter::new(1, Rand32::new(1));
-    assert!(limiter.check(0, 1, Instant::now()));
+    let now = Instant::now();
+    let mut limiter = RateLimiter::new(1, Rand32::new(1), now);
+    assert!(limiter.check(0, 1, now));
 }
 
 #[test]
 fn test_single_client() {
-    let mut limiter = RateLimiter::new(2, Rand32::new(1));
-    let mut client = SteadyClient::new(Key::Static(0), 1, 1);
-    simulate(1000, &mut limiter, &mut [&mut client]);
-    assert_eq!(1000, client.accepted_requests);
-    assert_eq!(0, client.rejected_request);
+    let mut limiter = RateLimiter::new(2, Rand32::new(1), Instant::now());
+    let client = Rc::new(RefCell::new(Client::new(Key::Static(0), 1, 1)));
+    simulate(1000, &mut limiter, &[Rc::clone(&client)]);
+    assert_eq!(1000, client.borrow().accepted_requests);
+    assert_eq!(0, client.borrow().rejected_request);
+}
+
+#[test]
+fn test_single_client_overload() {
+    let mut limiter = RateLimiter::new(1, Rand32::new(1), Instant::now());
+    let client = Rc::new(RefCell::new(Client::new(Key::Static(0), 1, 5)));
+    simulate(1000, &mut limiter, &[Rc::clone(&client)]);
+    assert_contains!(150..250, client.borrow().accepted_requests);
+    assert_contains!(750..850, client.borrow().rejected_request);
 }
 
 #[test]
 fn test_four_clients() {
-    let mut limiter = RateLimiter::new(10, Rand32::new(1));
-    let mut client0 = SteadyClient::new(Key::Static(0), 100, 1);
-    let mut client1 = SteadyClient::new(Key::Static(1), 50, 1);
-    let mut client2 = SteadyClient::new(Key::Static(2), 10, 1);
-    let mut client3 = SteadyClient::new(Key::Static(3), 1, 1);
+    let mut limiter = RateLimiter::new(10, Rand32::new(1), Instant::now());
+    let client0 = Rc::new(RefCell::new(Client::new(Key::Static(0), 100, 1)));
+    let client1 = Rc::new(RefCell::new(Client::new(Key::Static(1), 50, 1)));
+    let client2 = Rc::new(RefCell::new(Client::new(Key::Static(2), 10, 1)));
+    let client3 = Rc::new(RefCell::new(Client::new(Key::Static(3), 1, 1)));
     simulate(
         1000,
         &mut limiter,
-        &mut [&mut client0, &mut client1, &mut client2, &mut client3],
+        &[
+            Rc::clone(&client0),
+            Rc::clone(&client1),
+            Rc::clone(&client2),
+            Rc::clone(&client3),
+        ],
     );
-    assert_contains!(2000..3000, client0.accepted_requests);
-    assert_contains!(2000..3000, client1.accepted_requests);
-    assert_contains!(2000..3000, client2.accepted_requests);
-    assert_contains!(2000..3000, client3.accepted_requests);
+    assert_contains!(2000..3000, client0.borrow().accepted_requests);
+    assert_contains!(2000..3000, client1.borrow().accepted_requests);
+    assert_contains!(2000..3000, client2.borrow().accepted_requests);
+    assert_contains!(2000..3000, client3.borrow().accepted_requests);
     assert_contains!(
         9000..10_000,
-        client0.accepted_requests
-            + client1.accepted_requests
-            + client2.accepted_requests
-            + client3.accepted_requests
+        client0.borrow().accepted_requests
+            + client1.borrow().accepted_requests
+            + client2.borrow().accepted_requests
+            + client3.borrow().accepted_requests
     );
 }
 
 #[test]
 fn test_client_and_long_tail() {
-    let mut limiter = RateLimiter::new(10, Rand32::new(1));
-    let mut client = SteadyClient::new(Key::Static(0), 100, 1);
-    let mut longtail = SteadyClient::new(Key::Random(Cell::new(Rand32::new(2))), 5, 1);
-    simulate(1000, &mut limiter, &mut [&mut client, &mut longtail]);
-    assert_contains!(4500..5500, client.accepted_requests);
-    assert_contains!(4500..5500, longtail.accepted_requests);
-    assert_contains!(
-        9000..10_000,
-        client.accepted_requests + longtail.accepted_requests
+    let mut limiter = RateLimiter::new(10, Rand32::new(1), Instant::now());
+    let client = Rc::new(RefCell::new(Client::new(Key::Static(0), 5, 1)));
+    let longtail = Rc::new(RefCell::new(Client::new(
+        Key::Random(Cell::new(Rand32::new(2))),
+        5,
+        1,
+    )));
+    simulate(
+        1000,
+        &mut limiter,
+        &[Rc::clone(&client), Rc::clone(&longtail)],
     );
+    assert_contains!(4900..5000, client.borrow().accepted_requests);
+    assert_contains!(4900..5000, longtail.borrow().accepted_requests);
 }
