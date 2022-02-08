f51c4446831d3a04af791affe104439576d1d385
Michael Leonhard <michael@leonhardllc.com>
2022-02-07 18:07:47 -0800

    Upgrade check.sh and check-all.sh.

--- check-all.sh
+++ check-all.sh
@@ -5,17 +5,17 @@ cd "$(dirname "$0")"
 echo "PWD=$(pwd)"
 time (
   set -x
-  ./check.sh any-range
+  ./check.sh any-range "$@"
   ./check.sh build-data "$@"
-  ./check.sh build-data-test
-  ./check.sh fair-rate-limiter
-  ./check.sh permit
-  ./check.sh prob-rate-limiter
-  ./check.sh rustls-pin
-  ./check.sh safe-dns
-  ./check.sh safe-lock
-  ./check.sh temp-dir
-  ./check.sh temp-file
+  ./check.sh build-data-test "$@"
+  ./check.sh fair-rate-limiter "$@"
+  ./check.sh permit "$@"
+  ./check.sh prob-rate-limiter "$@"
+  ./check.sh rustls-pin "$@"
+  ./check.sh safe-dns "$@"
+  ./check.sh safe-lock "$@"
+  ./check.sh temp-dir "$@"
+  ./check.sh temp-file "$@"
   set +x
   echo -n "$(basename "$0") finished."
 )
